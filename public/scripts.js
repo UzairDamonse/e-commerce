21b13ab1c63e8bf03ce2fd74861747a1f1c8cf12
UzairDamonse <uzairdamonse03@gmail.com>
2022-08-04 09:32:28 +0200

    added heroku link

--- public/scripts.js
+++ public/scripts.js
@@ -66,16 +66,19 @@ function showUsers(users) {
 
 async function Login(e) {
   e.preventDefault();
-  const response = await fetch("http://localhost:6969/users/login", {
-    method: "POST",
-    body: JSON.stringify({
-      email: document.querySelector("#email").value,
-      password: document.querySelector("#password").value,
-    }),
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+  const response = await fetch(
+    "https://uzairs-e-commerce.herokuapp.com/users/login",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email: document.querySelector("#email").value,
+        password: document.querySelector("#password").value,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
   const data = await response.json();
   console.log(data);
   alert("Logged in successfully");
